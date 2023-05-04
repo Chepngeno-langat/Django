1c6efc80c05b08679fa0df2b98a97c6acec104a5
Chepngeno-langat <langatkarenn@gmail.com>
2023-05-04 22:37:05 +0300

    queries in django database

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Project
 
 projectsList = [
     {
@@ -21,16 +22,12 @@ projectsList = [
 ]
 
 def projects(request):
-    page = "projects"
-    number = 13
-    context = {'page': page, 'number': number, 'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-
-    return render(request, 'projects/single-projects.html', {'project':projectObj})
+    projectObj = Project.objects.get(id=pk)
+    tags = projectObj.tags.all()
+    return render(request, 'projects/single-projects.html', {'project':projectObj, 'tags':tags})
     
