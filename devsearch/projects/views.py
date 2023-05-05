d93dd3b26b6c2965da1815f728f95f92e4293005
Chepngeno-langat <langatkarenn@gmail.com>
2023-05-05 19:07:43 +0300

    django forms

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,24 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
 
 def projects(request):
     projects = Project.objects.all()
@@ -30,3 +14,7 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-projects.html', {'project':projectObj})
     
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
