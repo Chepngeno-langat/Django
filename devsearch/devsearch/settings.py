0af86469b927293296cfb0e5ae7bec379caaa4df
Chepngeno-langat <langatkarenn@gmail.com>
2023-05-08 20:45:16 +0300

    static files

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-)hwm^(()d3)w$j8^zgo#r$g_rck1u32&ewrhps_r%*yh_ew^l)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -32,6 +32,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +119,8 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
